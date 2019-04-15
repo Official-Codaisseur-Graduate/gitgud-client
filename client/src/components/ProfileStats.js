8a3b6eee3cbcaf2cdc45c22ec358d7e5814fd968
w3bgir1 <akulshyna@gmail.com>
2019-04-15 15:32:17 +0200

    edited display of profile stats

--- client/src/components/ProfileStats.js
+++ client/src/components/ProfileStats.js
@@ -1,15 +1,29 @@
 import * as React from "react";
 import "./ProfileStats.css";
 
-const spacecamel = string => {
-  const stringToLowerCase = string
-    .replace(/([a-z])([A-Z])/g, "$1 $2")
-    .toLowerCase();
-  return stringToLowerCase.charAt(0).toUpperCase() + stringToLowerCase.slice(1);
+const icon = value => {
+  if (value)
+    return (
+      <img
+        src={require("../img/checked.svg")}
+        alt="checked"
+        width="30"
+        height="30"
+      />
+    );
+  return (
+    <img
+      src={require("../img/cancel.svg")}
+      alt="not-checked"
+      width="30"
+      height="30"
+    />
+  );
 };
 
 export default function ProfileStats(props) {
-  console.log(props)
+  console.log(props);
+  const profileStats = props.user.profileStats;
   return (
     <section className="stats">
       <p className="stats__username"> username: {props.user.username} </p>
@@ -20,34 +34,42 @@ export default function ProfileStats(props) {
 
       <section className="stats__profile">
         <h2 className="stats__profile-header"> Profile statistics </h2>
-        <div className="stats__profile-container">
-          {props.user &&
-            props.user.profileStats !== 0 &&
-            Object.entries(props.user.profileStats).map(([key, value]) => {
-              if (key !== "__typename") {
-                return (
-                  <div className="stats__profile-item" key={key}>
-                    {value ? (
-                      <img
-                        src={require("../img/checked.svg")}
-                        alt="checked"
-                        width="30"
-                        height="30"
-                      />
-                    ) : (
-                      <img
-                        src={require("../img/cancel.svg")}
-                        alt="not-checked"
-                        width="30"
-                        height="30"
-                      />
-                    )}
-                    <p className="stats__item-name">{spacecamel(key)}</p>
-                  </div>
-                );
-              }
-            })}
-        </div>
+        {props.user && props.user.profileStats !== 0 && (
+          <div className="stats__profile-container">
+            <div className="stats__profile-item" key="bio">
+              {icon(profileStats.bio)}
+              <p className="stats__item-name">Bio</p>
+            </div>
+            <div className="stats__profile-item" key="email">
+              {icon(profileStats.email)}
+              <p className="stats__item-name">Email</p>
+            </div>
+            <div className="stats__profile-item" key="isHireable">
+              {icon(profileStats.isHireable)}
+              <p className="stats__item-name">Available for hire</p>
+            </div>
+            <div className="stats__profile-item" key="location">
+              {icon(profileStats.location)}
+              <p className="stats__item-name">Location</p>
+            </div>
+            <div className="stats__profile-item" key="name">
+              {icon(profileStats.name)}
+              <p className="stats__item-name">Full name</p>
+            </div>
+            <div className="stats__profile-item" key="picture">
+              {icon(profileStats.picture)}
+              <p className="stats__item-name">Propper profile photo</p>
+            </div>
+            <div className="stats__profile-item" key="website">
+              {icon(profileStats.websiteUrl)}
+              <p className="stats__item-name">URL to your website / LinkedIn</p>
+            </div>
+            <div className="stats__profile-item" key="pinnedRepositories">
+              {icon(profileStats.pinnedRepositories)}
+              <p className="stats__item-name">Pinned repositories</p>
+            </div>
+          </div>
+        )}
       </section>
       {props.user && props.user.stats.totalPinnedRepos !== null && (
         <section className="stats__average">
