af0bce5b3948c70b935c7be396356864d36d21bc
w3bgir1 <akulshyna@gmail.com>
2019-04-17 16:06:22 +0200

    added server url

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const url = `http://localhost:3030/graphql`
+const url = `https://gitgudserver.herokuapp.com/graphql`
 
 const httpLink = {
   uri: url
