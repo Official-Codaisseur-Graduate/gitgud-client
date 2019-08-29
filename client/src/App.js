b0ecfa74b0b6ce602a2681ce112859da8c5175f8
halynaburdiian <verdamtion@gmail.com>
2019-08-29 10:34:45 +0200

    Redirect

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import {Route} from 'react-router-dom'
 import FormContainer from './components/FormContainer';
 import FeedbackButton from './components/FeedbackButton';
 import ProfileStats from './components/ProfileStats';
+import LandingPage from './components/LandingPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
       <Route path='/' exact component={FormContainer}/>
-      <Route path='/:username' exact component={ProfileStats}/>
-      
+      <Route path='/:username'  exact component={ProfileStats}/>
       <FeedbackButton />
       </div>
     );
--- client/src/components/FormContainer.js
+++ client/src/components/FormContainer.js
@@ -2,20 +2,22 @@ import React from "react";
 import Form from "./Form";
 import "./Form.css";
 import LandingPage from './LandingPage'
-import { Query } from "react-apollo";
-import { GET_USER_DATA } from "../gql";
+import {Query} from "react-apollo";
+import {GET_USER_DATA} from "../gql";
 import ProfileStats from "./ProfileStats";
 import Loader from "./Loader";
-import {Link} from 'react-router-dom'
+import {withRouter} from "react-router";
+import {Redirect} from 'react-router-dom'
 
-export default class FormContainer extends React.Component {
+class FormContainer extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       search: "",
       username: "",
-      landingPage: true
+      landingPage: true,
+      redirect: false
     };
   }
 
@@ -25,28 +27,34 @@ export default class FormContainer extends React.Component {
 
   onSubmit = event => {
     event.preventDefault();
-    this.setState({username: this.state.search, landingPage: false});
-    this.props.history.push(`/${this.state.search}`)
+    this.setState({username: this.state.search, landingPage: false, redirect: true});
+    if (this.state.redirect === true) {
+      return <Redirect to={`/${this.state.username}`}/>
+    }
   };
 
   renderQuery = () => {
     return <Query
       query={GET_USER_DATA}
       skip={this.state.username === ``}
-      variables={{ username: this.state.username }}
-    >
-      {({ loading, error, data }) => {
+      variables={{
+      username: this.state.username
+    }}>
+      {({loading, error, data}) => {
         console.log("LOADING", loading)
-        if (loading) return <Loader />;
-
-        if (error)
+        if (loading) 
+          return <Loader/>;
+        
+        if (error) 
           return (
             <div className="errorBox">
-              <p>Please submit valid username </p>
+              <p>Please submit valid username
+              </p>
             </div>
           );
-
-        return <div>  {data && <ProfileStats user={data.user} />}</div>;
+        
+        return <div>
+          {data && <ProfileStats user={data.user}/>}</div>;
       }}
     </Query>
   }
@@ -72,26 +80,22 @@ export default class FormContainer extends React.Component {
             username={justName}
             onSubmit={this.onSubmit}
             onChange={this.onChange}
-            renderQuery={this.renderQuery}
-          />
+            renderQuery={this.renderQuery}/>
           <LandingPage/>
         </div>
       );
     } else 
       return <Form
-        username={
-          slash === -1
-            ? this.state.username
-            : justName
-        }
+        username={slash === -1
+        ? this.state.username
+        : justName}
         onSubmit={this.onSubmit}
         onChange={this.onChange}
-        reponame={
-          slash === -1
-            ? null
-            : justRepo
-        }
-        renderQuery={this.renderQuery}
-      />
+        reponame={slash === -1
+        ? null
+        : justRepo}
+        renderQuery={this.renderQuery}/>
   }
 }
+
+export default withRouter(FormContainer)
