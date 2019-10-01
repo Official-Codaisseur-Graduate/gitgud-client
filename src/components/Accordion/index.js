dcbdaae4387d745d328a1c36b55a72c51effbc0d
loklock <lokoschenko@gmail.com>
2019-10-01 17:52:05 +0200

    Add red to sections where score is 0

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -10,18 +10,28 @@ import {
 import "./Accordion.css";
 
 export default class RepoAccordion extends Component {
+
+  highlited = () => {
+    const arr = this.props.button.split(' ')
+    const score = Number(arr[arr.length-1])
+    if (score === 0) {
+      return 'highlited'
+    }
+    return ''
+  }
+
   render() {
     return (
-      <Accordion allowMultipleExpanded="false" allowZeroExpanded="true">
+      <Accordion className={`accordion ${this.highlited()}`} allowMultipleExpanded="false" allowZeroExpanded="true">
         <AccordionItem>
           <AccordionItemHeading>
             <AccordionItemButton>{this.props.button}</AccordionItemButton>
           </AccordionItemHeading>
           <AccordionItemPanel>
-            <p className="accordion__content">
+            <div className="accordion__content">
               {this.props.panel}
               <p>{this.props.link}</p>
-            </p>
+            </div>
           </AccordionItemPanel>
         </AccordionItem>
       </Accordion>
