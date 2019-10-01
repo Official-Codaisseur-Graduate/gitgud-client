8bead864ef5887beb22eeefc820cb3dbddf32179
andreeaCcss <andreeaccss@gmail.com>
2019-10-01 15:27:20 +0200

    Markdown fetch

--- src/components/Wiki.js
+++ src/components/Wiki.js
@@ -4,19 +4,35 @@ import ReactMarkdown from "react-markdown";
 import General from "../markdown/General.md";
 import Branches from "../markdown/Branches.md";
 import Commits from "../markdown/Commits.md";
-import Profil from "../markdown/Profile.md";
+import Profile from "../markdown/Profile.md";
 
 export default class Wiki extends Component {
   state = { markdown: "" };
 
   componentWillMount() {
-    fetch(General)
-      .then(res => res.text())
-      .then(text => this.setState({ markdown: text }));
+    const sectionToFetch = this.props.match.params.section;
+
+    if (sectionToFetch === "general") {
+      fetch(General)
+        .then(res => res.text())
+        .then(text => this.setState({ markdown: text }));
+    } else if (sectionToFetch === "branches") {
+      fetch(Branches)
+        .then(res => res.text())
+        .then(text => this.setState({ markdown: text }));
+    } else if (sectionToFetch === "commits") {
+      fetch(Commits)
+        .then(res => res.text())
+        .then(text => this.setState({ markdown: text }));
+    } else if (sectionToFetch === "profile") {
+      fetch(Profile)
+        .then(res => res.text())
+        .then(text => this.setState({ markdown: text }));
+    }
   }
+
   render() {
     const { markdown } = this.state;
-
     return (
       <div>
         <ReactMarkdown source={markdown} />;
