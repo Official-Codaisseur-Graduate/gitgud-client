14b045a4c1190537a20e4bc30d479301674843ba
andreeaCcss <andreeaccss@gmail.com>
2019-10-01 16:24:03 +0200

    Add Links for wiki page

--- src/components/BranchesAccordionContainer.js
+++ src/components/BranchesAccordionContainer.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import RepoAccordion from './Accordion'
+import React, { Component } from "react";
+import RepoAccordion from "./Accordion";
+import { Link } from "react-router-dom";
 
 export default class GeneralAccordionContainer extends Component {
   state = {
@@ -7,36 +8,49 @@ export default class GeneralAccordionContainer extends Component {
       {
         id: 1,
         button: `Master branch: ${this.props.branchScore.hasMasterBranch}`,
-        panel: "‘master’ is considered to be the main branch where the source code of HEAD always reflects a production-ready state."
-      }, {
+        panel:
+          "‘master’ is considered to be the main branch where the source code of HEAD always reflects a production-ready state.",
+        link: <Link to={`/wiki/branches`}>Learn more</Link>
+      },
+      {
         id: 2,
         button: `Development branch: ${this.props.branchScore.hasDevelopmentBranch}`,
-        panel: " ‘development’ is considered to be the main branch where the source code of HEAD always reflects a state with the latest delivered development changes for the next release. Some would call this the “integration branch”."
-      }, {
+        panel:
+          " ‘development’ is considered to be the main branch where the source code of HEAD always reflects a state with the latest delivered development changes for the next release. Some would call this the “integration branch”.",
+        link: <Link to={`/wiki/branches`}>Learn more</Link>
+      },
+      {
         id: 3,
         button: `Feature branch:  ${this.props.branchScore.hasFeatBranch}`,
-        panel: "feature branches are among the ‘supporting branches’. These branches are you to work on features and unlike the main branches, these branches always have a limited life time, since they will be removed eventually."
-      }, {
+        panel:
+          "feature branches are among the ‘supporting branches’. These branches are you to work on features and unlike the main branches, these branches always have a limited life time, since they will be removed eventually.",
+        link: <Link to={`/wiki/branches`}>Learn more</Link>
+      },
+      {
         id: 4,
         button: `Number of Branches: ${this.props.branchScore.hasThreeBranches}`,
-        panel: "You should have at least three branches on your repository."
-      }, {
+        panel: "You should have at least three branches on your repository.",
+        link: <Link to={`/wiki/branches`}>Learn more</Link>
+      },
+      {
         id: 5,
         button: `Naming:  ${this.props.branchScore.useDescriptiveNames}`,
-        panel: `All branches, including supporting branches have naming conventions. They should start with master, development, feature, bug, hotfix or junk. In this repository, ${this.props.branchScore.properNamesCount} out of ${this.props.branchScore.branchCount} branches are named properly`
+        panel: `All branches, including supporting branches have naming conventions. They should start with master, development, feature, bug, hotfix or junk. In this repository, ${this.props.branchScore.properNamesCount} out of ${this.props.branchScore.branchCount} branches are named properly`,
+        link: <Link to={`/wiki/branches`}>Learn more</Link>
       }
     ]
-  }
+  };
 
   render() {
-    return (
-      this.state.accordionItems.map(accordionItem => {
-        return <RepoAccordion
+    return this.state.accordionItems.map(accordionItem => {
+      return (
+        <RepoAccordion
           key={accordionItem.id}
           button={accordionItem.button}
           panel={accordionItem.panel}
+          link={accordionItem.link}
         />
-      })
-    )
+      );
+    });
   }
 }
--- src/components/RepoStats/index.js
+++ src/components/RepoStats/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './RepoStats.css';
+import { Link } from "react-router-dom"
 import posed from 'react-pose';
+
+import './RepoStats.css';
 
 const Content = posed.div({
   closed: { height: 0 },
@@ -48,19 +50,21 @@ export default class Tabs extends React.Component {
                         { item.descriptionDetails.tooShort ? <div className="content-wrapper">Description is too short.</div> : null}
                         { !item.descriptionDetails.includesDependencies ? <div className="content-wrapper">Description should include names of the most relevant dependencies you used.</div> : null}
                         { !item.descriptionDetails.tooShort && !item.descriptionDetails.tooLong? <div className="content-wrapper">Although there isn’t a format for a good repository description, it is a way to provide a summary of a where the project is about and which stack and tools are used.</div> : null}
-
+                      <Link to={`/wiki/general`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 2} className={this.state.open === i + 2 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Readme: {item.repoReadMe < 100 ? "0%" : '100%'}
                       <Content className="content" pose={this.state.open === i + 2 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> A README file is important to your repository because it tells other people why your project is useful, what they can do with your project, and how they can use it. Without a readme, no one is going to look at you code.
                         </div>
+                        <Link to={`/wiki/general`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 3} className={this.state.open === i + 3 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Gitignore: {item.gitIgnoreScore < 100 ? "0%" : '100%'}
                       <Content className="content" pose={this.state.open === i + 3 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> A .gitignore file is used to for GIT to know which files to ignore before making a commit. It is, for example, very important to include your /node_modelus because when running npm install to start your app node will install different versions depending on the computer you work.
                         </div>
+                        <Link to={`/wiki/general`}>Learn more</Link>
                       </Content>
                     </li>
                   </ul>
@@ -73,6 +77,7 @@ export default class Tabs extends React.Component {
                       <Content className="content" pose={this.state.open === i + 5 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> ‘master’ is considered to be the main branch where the source code of HEAD always reflects a production-ready state.
                         </div>
+                        <Link to={`/wiki/branches`}>Learn more</Link>
                       </Content>
                     </li>
 
@@ -80,24 +85,28 @@ export default class Tabs extends React.Component {
                       <Content className="content" pose={this.state.open === i + 6 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> ‘development’ is considered to be the main branch where the source code of HEAD always reflects a state with the latest delivered development changes for the next release. Some would call this the “integration branch”.
                         </div>
+                        <Link to={`/wiki/branches`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 7} className={this.state.open === i + 7 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Feature branches: {item.branchScore.hasFeatBranch}%
                       <Content className="content" pose={this.state.open === i + 7 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> feature branches are among the ‘supporting branches’. These branches are you to work on features and unlike the main branches, these branches always have a limited life time, since they will be removed eventually.
                         </div>
+                        <Link to={`/wiki/branches`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 8} className={this.state.open === i + 8 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Number of branches: {item.branchScore.hasThreeBranches}%
                       <Content className="content" pose={this.state.open === i + 8 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> You should have at least three branches on your repository.
                         </div>
+                        <Link to={`/wiki/branches`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 9} className={this.state.open === i + 9 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Proper branch names: {item.branchScore.useDescriptiveNames}%
                       <Content className="content" pose={this.state.open === i + 9 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> All branches, including supporting branches have naming conventions. They should start with master, development, feature, bug, hotfix or junk.
                         </div>
+                        <Link to={`/wiki/branches`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 4} className="stats-breakdown__points"> Total branches score: {item.branchScore.totalScore}%
@@ -112,24 +121,28 @@ export default class Tabs extends React.Component {
                       <Content className="content" pose={this.state.open === i + 11 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> If you have to add an “and” in your commit message, you’ve already committed too much.
                         </div>
+                        <Link to={`/wiki/commits`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 12} className={this.state.open === i + 12 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Commits with period: {item.commitScore.containsPeriod}%
                       <Content className="content" pose={this.state.open === i + 12 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> Don’t end the commit with a period. Save your characters.
                         </div>
+                        <Link to={`/wiki/commits`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 13} className={this.state.open === i + 13 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> Length of commits: {item.commitScore.lengthExceeds}%
                       <Content className="content" pose={this.state.open === i + 13 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> The commit title should be less than 50 characters. Shorter things are easier to read.
                         </div>
+                        <Link to={`/wiki/commits`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 14} className={this.state.open === i + 14 ? 'stats-breakdown__item--active' : 'stats-breakdown__item'}> First word of commit starting with uppercase {item.commitScore.upperCase}%
                       <Content className="content" pose={this.state.open === i + 14 ? 'open' : 'closed'}>
                         <div className="content-wrapper"> This is seen as generally good conduct by most gitsperts. Sentence case is also more professional looking and easier to read.
                         </div>
+                        <Link to={`/wiki/commits`}>Learn more</Link>
                       </Content>
                     </li>
                     <li data-id={i + 10} className="stats-breakdown__points"> Total commits score: {item.commitScore.totalScore}% </li>
